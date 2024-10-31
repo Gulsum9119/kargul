99da61e7dbc061bc6b35145ab2a926f65909f764
Gulsum <gmin9119@gmail.com>
2024-10-31 11:43:38 +0500

    new all direction

--- js/code.js
+++ js/code.js
@@ -6,3 +6,11 @@ document.addEventListener('DOMContentLoaded', function() {
         nav.classList.toggle('active'); // Переключаем класс active для навигации
     });
 });
+
+const menuBurger = document.querySelector('.menu-burger_header');
+const nav = document.querySelector('.nav ul');
+
+menuBurger.addEventListener('click', () => {
+    menuBurger.classList.toggle('active');
+    nav.classList.toggle('active');
+});
